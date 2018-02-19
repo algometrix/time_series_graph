e3d4d0a0ab7b0e0381c4b528379811daee05e6ab
Ashish Nagar <ashishnagar31@gmail.com>
2018-02-19 23:24:35 +0530

    Testing Changes

--- graph.py
+++ graph.py
@@ -4,4 +4,4 @@ import json
 def get_graph_data(**kwargs):
     name = kwargs['name']
     time = kwargs.get('time', None)
-    return json.dumps(list(get_data_from_db(name,time)))
+    return json.dumps(list(get_data_from_db(name,time))[0])
