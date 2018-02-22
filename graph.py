4fc89b985cfb6366cc5de6bf9730453fadb26a3c
Ashish Nagar <ashishnagar31@gmail.com>
2018-02-22 18:30:13 +0530

    Changes in api

--- graph.py
+++ graph.py
@@ -4,7 +4,7 @@ import json
 def get_graph_data(**kwargs):
     name = kwargs['name']
     time = kwargs.get('time', None)
-    return list(get_data_from_influx(name,time))[0]
+    return {'name' : name, 'values' : list(get_data_from_influx(name,time))[0] }
 
 def get_all_tables():
     return list(get_measurements_from_influx())[0]
